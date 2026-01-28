c8babd5822af4448a3cda837dc1dfcacd46f6d7e
Francisco Rodrigues <francisco.rodrigues@cern.ch>
2026-01-28 17:24:06 +0100

    Use rollup logging inside plugin

--- rollup-plugin-ts-flask-urls/src/index.ts
+++ rollup-plugin-ts-flask-urls/src/index.ts
@@ -1,4 +1,4 @@
-import { Plugin } from "rollup";
+import { Plugin, PluginContext } from "rollup";
 import { spawn, SpawnOptionsWithoutStdio } from "node:child_process";
 import path from "node:path";
 import fg from "fast-glob";
@@ -10,22 +10,26 @@ export interface TsFlaskUrlsPluginOptions {
     watchBackend?: boolean;
 }
 
-async function aspawn(command: string, args: readonly string[] | undefined, options?: SpawnOptionsWithoutStdio | undefined): Promise<number|null> {
+async function aspawn(command: string, args: readonly string[] | undefined, options?: SpawnOptionsWithoutStdio | undefined): Promise<{status: number|null, output: string}> {
     const child = spawn(command, args, options);
+    let output = "";
     const status = await new Promise<number|null>(resolve => {
+        for (const stream of [child.stderr, child.stdout]) {
+            stream.on("data", (chunk: string) => output += chunk);
+        }
         child.on("close", code => resolve(code));
         child.on("exit", code => resolve(code));
     });
-    return status;
+    return { status, output };
 }
 
-async function runCodegen(options: TsFlaskUrlsPluginOptions) {
+async function runCodegen(this: PluginContext, options: TsFlaskUrlsPluginOptions) {
     const {
         backendRoot,
         outDir,
     } = options;
 
-    const status = await aspawn(
+    const result = await aspawn(
         "flask",
         [
             "ts-flask-urls",
@@ -34,11 +38,21 @@ async function runCodegen(options: TsFlaskUrlsPluginOptions) {
         ],
         {
             cwd: path.resolve(backendRoot),
-            shell: process.platform === "win32"
+            shell: process.platform === "win32",
         }
     );
-    if (status !== 0) {
-        throw new Error(`ts-flask-urls codegen failed with status ${status}`);
+
+    for (const line of result.output.split("\n")) {
+        if (line.startsWith("Warning: ")) {
+            this.warn(line.substring(9));
+        }
+    }
+
+    if (result.status !== 0) {
+       this.warn(`codegen failed with status ${result.status}`);
+    }
+    else {
+        this.info("codegen finished")
     }
 }
 
@@ -54,11 +68,11 @@ export default function tsFlaskUrlsPlugin(options: TsFlaskUrlsPluginOptions): Pl
             for (const file of files) {
                 this.addWatchFile(file);
             }
-            await runCodegen(options);
+            await runCodegen.call(this, options);
         },
         async watchChange(id) {
             if (!id.startsWith(path.resolve(options.backendRoot))) return;
-            await runCodegen(options);
+            await runCodegen.call(this, options);
         }
     };
 }
